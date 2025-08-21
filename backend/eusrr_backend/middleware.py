a27be7620f8aa6b4e947a20744943fd00e55300d
Игорь Надеин <igor_26reg@mail.ru>
2025-08-21 08:05:44 +0300

    разрешение регистрирующих апи для Middleware

--- backend/eusrr_backend/middleware.py
+++ backend/eusrr_backend/middleware.py
@@ -1,25 +1,25 @@
 from django.shortcuts import redirect
 from django.urls import reverse
 
+
 class AuthRequiredMiddleware:
     """
     Разрешает доступ неавторизованным пользователям только к страницам входа и регистрации.
     """
+
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
         allowed_paths = [
-            reverse('login'),
-            reverse('employees:register'),
-            reverse('employees:email_verify'),
-            reverse('employees:resend_email'),
-            reverse('api_v1:register'),
-            reverse('api_v1:resend-email'),
-            reverse('api_v1:verify-email')
-            
-            
+            reverse("login"),
+            reverse("employees:register"),
+            reverse("employees:email_verify"),
+            reverse("employees:resend_email"),
+            reverse("api_v1:register"),
+            reverse("api_v1:resend-email"),
+            reverse("api_v1:verify-email"),
         ]
         if not request.user.is_authenticated and request.path not in allowed_paths:
-            return redirect('login')
+            return redirect("login")
         return self.get_response(request)
