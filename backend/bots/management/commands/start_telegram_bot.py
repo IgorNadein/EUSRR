0be01476faee387e620f6bc4e678dcdd0744ae73
lizerk <lizerk-dev@gmail.com>
2026-03-10 13:42:45 +0300

    очень много лишнего удалил в частности ботов и хикцентрал

--- backend/bots/management/commands/start_telegram_bot.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from bots.services.telegram import run_telegram_bot
-
-
-class Command(BaseCommand):
-    help = "Запуск Telegram-бота для рассылки и привязки сотрудников"
-
-    def handle(self, *args, **options):
-        run_telegram_bot()
